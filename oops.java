f2623f88a210dc7f999ae6757e04b67da7cae60a
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-07-04 19:14:57 +0530

    Add files via upload

---
+++ oops.java
@@ -0,0 +1,19 @@
+public class oops {
+    String name;
+    int rollno;
+
+    public  void display() {
+        System.out.println(name);
+        System.out.println(rollno);
+    }
+}
+class main{
+    public static void main(String[] args) {
+        oops out = new oops();
+        out.name="arun";
+        out.rollno = 1234;
+        out.display();
+
+
+    }
+}
