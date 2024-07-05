8baec12d3094bfa6fd606a07600fb5c1b0d57f41
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-07-05 20:43:41 +0530

    Add files via upload

---
+++ constroctor.java
@@ -0,0 +1,23 @@
+public class constroctor {
+    int rollno;
+    String name;
+    int mark;
+
+    constroctor(int rollno, int mark) {
+        System.out.println(rollno);
+        name="arun";
+        System.out.println(name);
+        System.out.println(mark);
+    }
+    public void disp(String[] args) {
+        System.out.println(rollno);
+        System.out.println(name);
+    }
+}
+class num{
+    public static void main(String[] args)
+    {
+        constroctor con=new constroctor(55,100);
+
+        }
+}
