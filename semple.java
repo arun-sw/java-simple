46b3d30ea7768027e35ecbab44187a19cce8b39a
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-07-05 20:44:55 +0530

    Add files via upload

---
+++ semple.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class semple {
+    public static  void main(String[] args) {
+
+        Scanner reader=new Scanner(System.in);
+        System.out.println("enter the nmber");
+        int number= reader.nextInt();
+        System.out.println("out:"+number);
+    }
+}
