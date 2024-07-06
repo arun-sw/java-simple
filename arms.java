1c8d236eb17f6936c2414a6a6aca2427610b81ea
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-07-06 19:19:03 +0530

    Add files via upload

---
+++ arms.java
@@ -0,0 +1,18 @@
+public class arms {
+    public static void main(String[] args) {
+        int a,n=153,sum=0,copy = 0;
+
+        n=copy;
+        while (n!=0){
+            a= 0;
+            sum+= 0;
+            n= 0;
+        }
+        if(n==sum){
+            System.out.println("it's armstrong number");
+        }
+        else {
+            System.out.println("it's not armstrong number");
+        }
+    }
+}
