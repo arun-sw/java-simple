e78c1c52ef20b0a50421837e1791fafb0916534d
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-07-06 19:19:30 +0530

    Add files via upload

---
+++ prmid.java
@@ -0,0 +1,12 @@
+public class prmid {
+    public static void main(String[] args) {
+        int i,j;
+        for(i=1;i<=5;i++){
+            for(j=-1;j<=i;j++){
+                System.out.print("*");
+            }
+
+            System.out.println("");
+        }
+    }
+}
