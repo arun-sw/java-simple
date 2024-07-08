a3af75dbf1be3c418fa3aa40f4bea7eebe98c599
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-07-08 19:33:58 +0530

    Update inga.java

--- inga.java
+++ inga.java
@@ -2,9 +2,9 @@ public class inga{
     public static void main(String[] args) {
 //        animal ani =new animal();
 //       dog do = new dog();
-        human hum = new human();
-        System.out.println(hum.legs);
-        System.out.println(hum.iam);
+        human h = new human();
+        System.out.println(h.legs);
+        System.out.println(h.iam);
     }
 }
 class animal{
