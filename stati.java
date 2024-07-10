039799ffebb3c715c50cff8fa998a3ebbb22dcdd
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-07-10 19:05:21 +0530

    Add files via upload

---
+++ stati.java
@@ -0,0 +1,25 @@
+public class stati {
+    public static void main(String[] args) {
+        lion l=new lion();
+        l.tiger();
+        l.dog();
+      forest.allanimals();
+        System.out.println(lion.lionhave);
+
+    }
+}
+class forest{
+   static   void allanimals(){
+         System.out.println("All animals");
+     }
+    void dog(){
+         System.out.println("ieat");
+    }
+
+}
+class lion extends forest{
+   static int lionhave=4;
+    void tiger(){
+        System.out.println("keep");
+    }
+}
