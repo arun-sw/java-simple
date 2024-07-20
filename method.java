a68580bddbd2b312a4e992fb67e5db962f5747aa
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-07-20 19:24:18 +0530

    Update method.java

--- method.java
+++ method.java
@@ -1,11 +1,9 @@
 public class method {
     public static void main(String[] args) {
-        add();
+        add(45,55);
     }
-
-    public static void add(){
-        int a=5;
-        int b=6;
+    public static void add(int a,int b){
+        
         int c=a+b;
         System.out.println(c);
 
