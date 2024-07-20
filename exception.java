4902a2134e70d26f667dddf35844f19b23543b0a
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-07-20 19:25:22 +0530

    Update exception.java

--- exception.java
+++ exception.java
@@ -1,13 +1,12 @@
 public class exception {
     public static void main(String[] args) {
         int a =3;
-        int b=0;
-
+        int b=6;
         try {
             System.out.println(a/b);
         }
         catch(ArithmeticException e) {
-            System.out.println(a);
+            System.out.println(a+5);
         }
         System.out.println("hello");
     }
