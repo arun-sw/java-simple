7e7c733720acecdc04bc30d5c905909c63786dc5
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-07-21 19:48:33 +0530

    Update userdif.java

--- userdif.java
+++ userdif.java
@@ -1,13 +1,10 @@
 import java.util.Scanner;
-
 public class userdif extends Exception{
     userdif(String str){
         super(str);
     }
-
 } class target {
     static void animal(int leg) throws userdif{
-
         if(leg<=2)
         {
             throw new ArithmeticException("it's are human");
@@ -24,7 +21,7 @@ public class userdif extends Exception{
             animal(leg);
         }
         catch (ArithmeticException d){
-            System.out.println(d);
+            System.out.println(d.getmassage);
         }
         catch (userdif d){
             System.out.println(d.getmassage);
