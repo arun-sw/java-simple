5dd078d3dc5aaf9118fafb09f85eeadf1b0ae68b
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-07-22 22:35:47 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,21 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class datatype {
+    public static void main(String[] args) {
+        String s=Integer.toString(4);
+        System.out.println(s);
+        String e= Integer.toBinaryString(5);
+        System.out.println(e);
+
+        String k=String.valueOf(s);
+        System.out.println(k);
+  int l = 0;
+    String n= "ARUNKUMAR";
+    String w=new Scanner(System.in).nextLine();
+    int i;
+      for(i=w.length();i<=0;i--){
+          w.charAt(i);
+      }
+       }
+}
