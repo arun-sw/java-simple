3bc6d4a525dda73a58c668322921b3a3165ef940
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-07-22 22:38:40 +0530

    Update datatype.java

--- datatype.java
+++ datatype.java
@@ -5,15 +5,8 @@ public class datatype {
         System.out.println(s);
         String e= Integer.toBinaryString(5);
         System.out.println(e);
-
         String k=String.valueOf(s);
         System.out.println(k);
-  int l = 0;
-    String n= "ARUNKUMAR";
-    String w=new Scanner(System.in).nextLine();
-    int i;
-      for(i=w.length();i<=0;i--){
-          w.charAt(i);
       }
        }
 }
