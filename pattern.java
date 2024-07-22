fa347949ed5a3012ed5b51806f415724e68cd4f4
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-07-22 22:40:26 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 public class pattern {
     public static void main(String[] args) {
         Scanner sn =new Scanner(System.in);
-        System.out.println("enter the square velue");
+        System.out.println("enter the value");
                int n=sn.nextInt();
         for (int i=n;i>=1;i--){
             for (int j=1;j<=n;j++){
-                System.out.print(i);
+                System.out.print(n);
             }
             System.out.println();
         }
