71e0f6307b5e863156aafb88d5ecb3fbc0702fd6
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-07-23 19:12:33 +0530

    Update inharitance.java

--- inharitance.java
+++ inharitance.java
@@ -2,7 +2,7 @@ public class inharitance {
     int whool=40;
 
     public void collar(){
-        System.out.println("red");
+        System.out.println("block");
     }
     public void prise(){
         System.out.println("500");
