2c4cd4516f3d4601c5fea5cca2cae1e24ee905c6
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-07-24 22:09:30 +0530

    Add files via upload

---
+++ runnable.java
@@ -0,0 +1,21 @@
+    class daily implements Runnable{
+        public void run(){
+            System.out.println("I'm going to clg "+ Thread.currentThread().getId());
+            System.out.println("i am study "+ Thread.currentThread().getId());
+            System.out.println("i am came "+ Thread.currentThread().getId());
+            System.out.println("i am running "+ Thread.currentThread().getId());
+        }
+    }
+    public class runnable{
+        public static void main(String[] args) {
+            int i;
+            for (i=0;i<4;i++){
+                daily d =new daily();
+                Thread dol = new Thread(d);
+                dol.start();
+            }
+
+        }
+    }
+
+
