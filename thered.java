8fbb0333128b6db1dfba91d3f51fefaf56a0c110
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-07-24 22:10:11 +0530

    Add files via upload

---
+++ thered.java
@@ -0,0 +1,20 @@
+
+ class dailyrotine extends Thread
+{
+public void run(){
+    System.out.println("I'm going to clg "+dailyrotine.currentThread().getId());
+    System.out.println("i am study "+dailyrotine.currentThread().getId());
+    System.out.println("i am came "+dailyrotine.currentThread().getId());
+    System.out.println("i am running "+dailyrotine.currentThread().getId());
+}
+}
+public class thered {
+    public static void main(String[] args) {
+        int i;
+        for (i=0;i<4;i++){
+            dailyrotine dol = new dailyrotine();
+            dol.start();
+        }
+
+    }
+}
