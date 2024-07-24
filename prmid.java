44b636af7351223d976bc074d2f76c63619d65be
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-07-24 22:11:51 +0530

    Update prmid.java

--- prmid.java
+++ prmid.java
@@ -3,9 +3,8 @@ public class prmid {
         int i,j;
         for(i=1;i<=5;i++){
             for(j=-1;j<=i;j++){
-                System.out.print("*");
+                System.out.print("i");
             }
-
             System.out.println("");
         }
     }
