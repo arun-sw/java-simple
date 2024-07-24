0d88979a9d82d029d87c699c378e266f496fd61f
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-07-24 22:11:16 +0530

    Update semple.java

--- semple.java
+++ semple.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 public class semple {
     public static  void main(String[] args) {
-
         Scanner reader=new Scanner(System.in);
         System.out.println("enter the nmber");
         int number= reader.nextInt();
