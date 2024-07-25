b9c82da51d0a9cdbe9a8c8e9bd872f7df8115ac2
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-07-25 19:36:05 +0530

    Add files via upload

---
+++ sync.java
@@ -0,0 +1,36 @@
+class bottle{
+     int countbottle=0;
+    public synchronized void incrementbottle(){
+         countbottle++;
+     }
+}
+class team implements Runnable {
+    bottle counter;
+    team(bottle bot) {
+        this.counter = bot;
+    }
+       public void run(){
+            for(int i=1;i<=1000;i++){
+                counter.incrementbottle();
+            }
+        }
+    }
+class sync{
+    public static void main(String[] args) {
+        bottle counter=new bottle();
+        Thread team1=new Thread(new team(counter));
+        Thread team2=new Thread(new team(counter));
+        team1.start();
+        team2.start();
+
+        try {
+            team1.join();
+            team2.join();
+        }
+        catch(Exception a){
+            System.out.println(a);
+        }
+        System.out.println(counter.countbottle);
+
+    }
+}
