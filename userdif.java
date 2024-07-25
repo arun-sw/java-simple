52dfae85fc5388fcd4273b38337e0b780afab852
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-07-25 19:39:54 +0530

    Update userdif.java

--- userdif.java
+++ userdif.java
@@ -7,7 +7,7 @@ public class userdif extends Exception{
     static void animal(int leg) throws userdif{
         if(leg<=2)
         {
-            throw new ArithmeticException("it's are human");
+            throw new ArithmeticException("it's is human");
         }
         else {
             throw new userdif("it's are not human");
@@ -16,7 +16,7 @@ public class userdif extends Exception{
     public static void main(String[] args) {
         try
         {
-            System.out.println("Enter the legs");
+            System.out.println("Enter the leggs");
             int leg=new Scanner(System.in).nextInt();
             animal(leg);
         }
