814b68a38fe34249f35c9ed47b43115e2635b5ae
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-09-09 19:52:36 +0530

    Update datatype.java

--- datatype.java
+++ datatype.java
@@ -1,8 +1,6 @@
 import java.util.Scanner;
 public class datatype {
-
     public static void main(String[] args) {
-    
         String s=Integer.toString(4);
         System.out.println(s);
         String e= Integer.toBinaryString(5);
