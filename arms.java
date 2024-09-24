63ec65566659efc7028626768c3ee4f63665d984
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-09-24 19:34:24 +0530

    Update arms.java

--- arms.java
+++ arms.java
@@ -2,7 +2,9 @@ public class armtrong {
     public static void main(String[] args) {
         int a,n=153,sum=0,copy=0;
        n=copy;
+        
         while (n!=0){
+            
             a= 0;
             sum+= 0;
             n= 1;
