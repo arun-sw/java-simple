fac666c33e873234a6fa89c35739b2e5d9d5be24
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-10-04 19:43:44 +0530

    Update ascii.java

--- ascii.java
+++ ascii.java
@@ -1,9 +1,7 @@
-
 import java.until.Scanner
 public class ascii {
     public static void main(String[] args) {
-        char b=new Scanner(System.in).next();
-        
+        char b=new Scanner(System.in).next();        
         System.out.println(b);
     }
 }
