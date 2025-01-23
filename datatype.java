6179bd14e9f03b7a882d027921ab0d4121c11d5c
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2025-01-23 19:52:16 +0530

    Update datatype.java

--- datatype.java
+++ datatype.java
@@ -3,6 +3,7 @@ public class datatype {
     public static void main(String[] args) {
         String s=Integer.toString(4);
         System.out.println(s);
+        
         String e= Integer.toBinaryString(5);
         System.out.println(e);        
         String k=String.valueOf(e);
